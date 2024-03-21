a7d6a098de0dab09fb66b9ed63a128d7c15fb1f5
rickymagner <81349869+rickymagner@users.noreply.github.com>
2024-03-21 16:04:35 -0400

    Replace deploy_app with get_app_server for use with gunicorn

--- quickboard/app.py
+++ quickboard/app.py
@@ -17,29 +17,29 @@ def generate_layout(board):
 
     return layout
 
-
-def start_app(board, theme=dbc.themes.BOOTSTRAP, jupyter_mode='external', host=os.getenv("HOST", "127.0.0.1"),
-              port=8050, proxy=None, debug=True, app_title="Dash", **flask):
-    """
-    Takes an app instance and configures its board layout, then runs the app on given port. Extra args get sent to Flask server.
-    Theme should be selected from dbc.themes.
-    Other nice themes: DARKLY, CYBORG, BOOTSTRAP, FLATLY, LUX, LUMEN, SOLAR.
-    """
-
+def create_app(board, theme=dbc.themes.BOOTSTRAP, app_title="Dash"):
     app = dash.Dash(__name__, external_stylesheets=[theme], title=app_title)
     app.config.suppress_callback_exceptions = True
 
     app.layout = generate_layout(board)
+    return app
 
+
+def start_app(board, theme=dbc.themes.BOOTSTRAP, jupyter_mode='external', host=os.getenv("HOST", "127.0.0.1"),
+              port=8050, proxy=None, debug=True, app_title="Dash", **flask):
+    """
+    Takes a Quickboard object and creates app with layout, then runs the app on given port.
+    Extra args get sent to Flask server. Theme should be selected from dbc.themes.
+    Other nice themes: DARKLY, CYBORG, BOOTSTRAP, FLATLY, LUX, LUMEN, SOLAR.
+    """
+    app = create_app(board=board, theme=theme, app_title=app_title)
     app.run(host=host, jupyter_mode=jupyter_mode, port=port, proxy=proxy, debug=debug, **flask)
 
 
-# def deploy_app(board):
-#     """
-#     This method can be used as an alternative to the above for running the app in a production environment, e.g. with
-#     gunicorn, using the server variable.
-#     """
-#     app.layout = generate_layout(board)
-#
-#     global server
-#     server = app.server
+def get_app_server(board, theme=dbc.themes.BOOTSTRAP, app_title="Dash"):
+    """
+    This method can be used as an alternative to the above for running the app in a production environment, e.g. with
+    gunicorn, using the server variable.
+    """
+    app = create_app(board=board, theme=theme, app_title=app_title)
+    return app.server
